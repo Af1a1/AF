ab81337a225fca512311204275bb79a85339962a
Af1a1 <aflalahamed0124@gmail.com>
2022-08-25 08:46:44 +0530

    Some changes

---
+++ client/src/components/product/productList.js
@@ -0,0 +1,152 @@
+import React from 'react';
+
+export default function productList() {
+  return (
+    <div className='row'>
+      <div className='col-md-3 col-sm-6'>
+        <div className='product-grid2'>
+          <div className='product-image2'>
+            {/* <a href="#">
+                        <img className="pic-1" src="https://www.w3schools.com/bootstrap4/img_avatar4.png">
+                        <img className="pic-2" src="https://www.w3schools.com/bootstrap4/img_avatar3.png">
+                    </a> */}
+            <ul className='social'>
+              <li>
+                <a href='#' data-tip='Quick View'>
+                  <i className='fa fa-eye'></i>
+                </a>
+              </li>
+              <li>
+                <a href='#' data-tip='Add to Wishlist'>
+                  <i className='fa fa-shopping-bag'></i>
+                </a>
+              </li>
+              <li>
+                <a href='#' data-tip='Add to Cart'>
+                  <i className='fa fa-shopping-cart'></i>
+                </a>
+              </li>
+            </ul>
+            <a className='add-to-cart' href=''>
+              Add to cart
+            </a>
+          </div>
+          <div className='product-content'>
+            <h3 className='title'>
+              <a href='#'>Women's Designer Top</a>
+            </h3>
+            <span className='price'>$599.99</span>
+          </div>
+        </div>
+      </div>
+      <div className='col-md-3 col-sm-6'>
+        <div className='product-grid2'>
+          <div className='product-image2'>
+            {/* <a href="#">
+                      <img className="pic-1" src="https://www.w3schools.com/bootstrap4/img_avatar4.png">
+                      <img className="pic-2" src="https://www.w3schools.com/bootstrap4/img_avatar3.png">
+                  </a> */}
+            <ul className='social'>
+              <li>
+                <a href='#' data-tip='Quick View'>
+                  <i className='fa fa-eye'></i>
+                </a>
+              </li>
+              <li>
+                <a href='#' data-tip='Add to Wishlist'>
+                  <i className='fa fa-shopping-bag'></i>
+                </a>
+              </li>
+              <li>
+                <a href='#' data-tip='Add to Cart'>
+                  <i className='fa fa-shopping-cart'></i>
+                </a>
+              </li>
+            </ul>
+            <a className='add-to-cart' href=''>
+              Add to cart
+            </a>
+          </div>
+          <div className='product-content'>
+            <h3 className='title'>
+              <a href='#'>Women's Yellow Top</a>
+            </h3>
+            <span className='price'>$699.99</span>
+          </div>
+        </div>
+      </div>
+      <div className='col-md-3 col-sm-6'>
+        <div className='product-grid2'>
+          <div className='product-image2'>
+            {/* <a href="#">
+                        <img className="pic-1" src="https://www.w3schools.com/bootstrap4/img_avatar4.png">
+                        <img className="pic-2" src="https://www.w3schools.com/bootstrap4/img_avatar3.png">
+                    </a> */}
+            <ul className='social'>
+              <li>
+                <a href='#' data-tip='Quick View'>
+                  <i className='fa fa-eye'></i>
+                </a>
+              </li>
+              <li>
+                <a href='#' data-tip='Add to Wishlist'>
+                  <i className='fa fa-shopping-bag'></i>
+                </a>
+              </li>
+              <li>
+                <a href='#' data-tip='Add to Cart'>
+                  <i className='fa fa-shopping-cart'></i>
+                </a>
+              </li>
+            </ul>
+            <a className='add-to-cart' href=''>
+              Add to cart
+            </a>
+          </div>
+          <div className='product-content'>
+            <h3 className='title'>
+              <a href='#'>Women's Designer Top</a>
+            </h3>
+            <span className='price'>$599.99</span>
+          </div>
+        </div>
+      </div>
+      <div className='col-md-3 col-sm-6'>
+        <div className='product-grid2'>
+          <div className='product-image2'>
+            {/* <a href="#">
+                        <img className="pic-1" src="https://www.w3schools.com/bootstrap4/img_avatar4.png">
+                        <img className="pic-2" src="https://www.w3schools.com/bootstrap4/img_avatar3.png">
+                    </a> */}
+            <ul className='social'>
+              <li>
+                <a href='#' data-tip='Quick View'>
+                  <i className='fa fa-eye'></i>
+                </a>
+              </li>
+              <li>
+                <a href='#' data-tip='Add to Wishlist'>
+                  <i className='fa fa-shopping-bag'></i>
+                </a>
+              </li>
+              <li>
+                <a href='#' data-tip='Add to Cart'>
+                  <i className='fa fa-shopping-cart'></i>
+                </a>
+              </li>
+            </ul>
+            <a className='add-to-cart' href=''>
+              Add to cart
+            </a>
+          </div>
+          <div className='product-content'>
+            <h3 className='title'>
+              <a href='#'>Women's Designer Top</a>
+            </h3>
+            <span className='price'>$599.99</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
